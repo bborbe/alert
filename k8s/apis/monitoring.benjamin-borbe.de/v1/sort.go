2a3aa277c37ba4fa71ab34db4fb03a9800aec4db
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-05-20 14:11:17 +0200

    add sort

---
+++ k8s/apis/monitoring.benjamin-borbe.de/v1/sort.go
@@ -0,0 +1,36 @@
+package v1
+
+import "strings"
+
+type AlertsSorted []Alert
+
+func (b AlertsSorted) Len() int {
+	return len(b)
+}
+
+func (b AlertsSorted) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+func (b AlertsSorted) Less(i, j int) bool {
+	return CompareAlert(b[i], b[j]) < 0
+}
+
+func CompareAlert(a, b Alert) int {
+	if result := strings.Compare(a.Namespace, b.Namespace); result != 0 {
+		return result
+	}
+	if result := strings.Compare(a.Name, b.Name); result != 0 {
+		return result
+	}
+	if result := strings.Compare(a.Spec.Name, b.Spec.Name); result != 0 {
+		return result
+	}
+	if result := strings.Compare(a.Spec.For, b.Spec.For); result != 0 {
+		return result
+	}
+	if result := strings.Compare(a.Spec.Expression, b.Spec.Expression); result != 0 {
+		return result
+	}
+	return 0
+}
