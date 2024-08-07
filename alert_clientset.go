15ebc358f841984bbc58dd6a7ac0e6f5c10989e2
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-08-07 16:19:51 +0200

    refactor

--- alert_clientset.go
+++ alert_clientset.go
@@ -5,27 +5,18 @@
 package alert
 
 import (
-	"github.com/golang/glog"
+	"context"
+
+	"github.com/bborbe/k8s"
 	"github.com/pkg/errors"
-	k8s_rest "k8s.io/client-go/rest"
-	k8s_clientcmd "k8s.io/client-go/tools/clientcmd"
 
 	"github.com/bborbe/alert/k8s/client/clientset/versioned"
 )
 
-func CreateClientset(kubeconfig string) (versioned.Interface, error) {
-	config, err := CreateConfig(kubeconfig)
+func CreateClientset(ctx context.Context, kubeconfig string) (versioned.Interface, error) {
+	config, err := k8s.CreateConfig(kubeconfig)
 	if err != nil {
 		return nil, errors.Wrap(err, "create k8s config failed")
 	}
 	return versioned.NewForConfig(config)
-}
-
-func CreateConfig(kubeconfig string) (*k8s_rest.Config, error) {
-	if len(kubeconfig) > 0 {
-		glog.V(4).Infof("create kube config from flags")
-		return k8s_clientcmd.BuildConfigFromFlags("", kubeconfig)
-	}
-	glog.V(4).Infof("create in cluster kube config")
-	return k8s_rest.InClusterConfig()
 }
